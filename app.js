36d36ec68115edb79bb1eb24ae8e47b434e13e89
Tim <ethantim123@gmail.com>
2021-02-23 19:00:21 +0800

    create view folder

--- app.js
+++ app.js
@@ -6,16 +6,28 @@
 */
 
 const express = require("express");
-const app = expree();
+const app = express();
 const ejs = require("ejs");
 
 // middleware
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
-    res.send("This is homepage.");
+    // res.send("This is homepage.");
+    res.render("index.ejs");
 });
 
+app.get("/response", (req, res) =>{
+    // console.log(req.query); //{ fullname: 'peterWang', age: '25' }
+    let { fullname, age } = req.query;
+    res.send("thanks for sending request.");
+});
+
+//示範如何傳值給view
+// app.get("/:name", (req, res) => {
+//     let { name } = req.params;
+//     res.render("person.ejs", {name : name}); // 當物件key與value相同時,可簡略為{ name }即可表示
+// });
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000.");
