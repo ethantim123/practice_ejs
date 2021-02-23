619d54cd92fbbf41fe63fbce925756e55c05f3df
Tim <ethantim123@gmail.com>
2021-02-23 19:19:34 +0800

    update form in ejs

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ const ejs = require("ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
+    const languages = [
+        { name: "Python", rating: 9.5, popularity: 9.7, trending: "super hot" },
+        { name: "Java", rating: 8.6, popularity: 8.2, trending: "same" },
+        { name: "C++", rating: 6.6, popularity: 7.7, trending: "same" },
+        { name: "PHP", rating: 2.5, popularity: 4.7, trending: "decreasing" },
+        { name: "JavaScript", rating: 8.5, popularity: 8.1, trending: "same" },
+    ];
+    
     // res.send("This is homepage.");
     res.render("index.ejs");
 });
@@ -20,7 +28,8 @@ app.get("/", (req, res) =>{
 app.get("/response", (req, res) =>{
     // console.log(req.query); //{ fullname: 'peterWang', age: '25' }
     let { fullname, age } = req.query;
-    res.send("thanks for sending request.");
+    // res.send("thanks for sending request.");
+    res.render("respond.ejs", {fullname, age});
 });
 
 //示範如何傳值給view
